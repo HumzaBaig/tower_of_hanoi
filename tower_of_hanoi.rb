7a7aa69a94478a59bd32ea176ac33adfe8e5cde0
Humza <HumzaCodeMail@gmail.com>
2016-05-04 20:57:52 -0500

    Add Narration Intro

---
+++ tower_of_hanoi.rb
@@ -0,0 +1,15 @@
+def narration
+	puts "\n"
+	puts " _____________________________________ "
+	puts "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
+	puts "|     Welcome to Tower of Hanoi!      |"
+	puts "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
+	puts "| Please choose one of the following: |"
+	puts "|                                     |\n"
+	puts "|(1) Instructions                     |"
+	puts "|(2) Start Game!                      |"
+	puts "|_____________________________________|"
+	puts "\n"
+end
+
+narration
